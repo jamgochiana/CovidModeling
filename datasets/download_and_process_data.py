3f052d0c3dfee255c13dc36502745d85b0ac44d5
Arec <jamgochian95@gmail.com>
2021-05-13 20:36:09 -0700

    download datasets working, starting processing scripts

--- datasets/download_and_process_data.py
+++ datasets/download_and_process_data.py
@@ -1,5 +1,48 @@
 # download_and_process_data.py
-import sys
+import sys, os
+import requests
 sys.path.append('./')
+import pandas as pd
+import numpy as np
+import datasets.process_utils.utils
 
+# urls
+hospitalizations = "https://data.sfgov.org/api/views/nxjg-bhem/rows.csv" #?accessType=DOWNLOAD
+cases_deaths = "https://data.sfgov.org/api/views/tvq9-ec9w/rows.csv" #?accessType=DOWNLOAD
 
+# download path
+pathdir = './datasets/raw/'
+if not os.path.exists(pathdir):
+    os.mkdir(pathdir)
+
+# hospitalizations
+if not os.path.exists(pathdir+'hospitalizations.csv'):
+    req = requests.get(hospitalizations)
+    url_content = req.content
+    csv_file = open(pathdir+'hospitalizations.csv', 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
+
+# cases and deaths
+if not os.path.exists(pathdir+'cases_deaths.csv'):
+    req = requests.get(cases_deaths)
+    url_content = req.content
+    csv_file = open(pathdir+'cases_deaths.csv', 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
+
+    
+# process data
+hosp = pd.read_csv(pathdir+'hospitalizations.csv')
+c_d = pd.read_csv(pathdir+'cases_deaths.csv')
+
+h = utils.process_hospitalizations(hosp)
+c, d = utils.process_cases_deaths(c_d)
+x = np.stack((c, h, d))
+
+# save out
+processdir = './datasets/processed/'
+if not os.path.exists(processdir):
+    os.mkdir(processdir)
+np.savetxt(processdir+'sf.csv', x, delimiter=',')
+    
--- datasets/process_utils/utils.py
+++ datasets/process_utils/utils.py
@@ -1,1 +1,31 @@
 # utils.py
+import pandas as pd
+import numpy as np
+
+def process_hospitalizations(hosp):
+    """
+    Process the hospitalization dataframe
+    
+    Args:
+        hosp (pd.DataFrame): hospitalization dataframe
+    Returns:
+        h (np.array): (days,)-sized array of hospitalizations
+    """
+    
+    pass
+    # return h
+
+
+
+def process_cases_deaths(c_d):
+    """
+    Process the cases and deaths dataframe
+    
+    Args:
+        c_d (pd.DataFrame): cases and deaths by cause dataframe
+    Returns:
+        c (np.array): (days,)-sized array of confirmed cases
+        d (np.array): (days,)-sized array of deaths
+    """    
+    pass
+    # return c, d
