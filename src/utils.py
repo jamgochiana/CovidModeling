adding moving averaging, fixing dates to return pd.datetimes for better plotting

src/utils.py
@@ -10,12 +10,12 @@ def read_csv(path):
         path (string): path to data csv
     Returns:
         data (np.array): (n_times, n_vars)-sized array of COVID data
-        dates (list of strings): list of dates
+        dates (pd.Series): (n_times)-sized series of pd.datetimes
         columns (list of strings): list of column names
   
     """
     df = pd.read_csv(path)
-    dates = list(df[:,0].values)
+    dates = pd.to_datetime(df.iloc[:,0].values)
     columns = list(df.columns.values[1:])
     data = df.iloc[:,1:].to_numpy()
     return data, dates, columns
@@ -27,13 +27,17 @@ def moving_average(data, dates, days=7):
     
     Args:
         data (np.array): (n_times, n_vars)-sized array of COVID data
-        dates (list): (n_times)-sized list of date strings
+        dates (pd.Series): (n_times)-sized series of pd.datetimes
         days (int): number of days to average over
     Returns:
-        mva (np.array): (n_times-days+1, n_vars)-sized array of COVID data  
-        mva_dates (list): (n_times-days+1)-sized list of date strings
+        mva_data (np.array): (n_times-days+1, n_vars)-sized array of COVID data
+        mva_dates (pd.Series): (n_times-days+1)-sized series of pd.datetimes
     """
-    pass
+    T, V = data.shape
+    mva_dates = dates[(days-1):]
+    stacked = np.stack([data[i:T-days+i+1] for i in range(days)])
+    mva_data = stacked.mean(axis=0)
+    return mva_data, mva_dates
 
 def train_test_split(data, dates, test_days=21):
     """
@@ -45,8 +49,8 @@ def train_test_split(data, dates, test_days=21):
     Returns:
         train (np.array): (n_times-test_days, n_vars)-sized array of training COVID data    
         test (np.array): (test_days, n_vars)-sized array of testing COVID data
-        train_dates (list): (n_times-test_days)-sized list of date strings    
-        test_dates (list): (test_days)-sized list of date strings
+        train_dates (pd.Series): (n_times-test_days)-sized series of pd.datetimes    
+        test_dates (pd.Series): (test_days)-sized series of pd.datetimes
     """
     T, V = data.shape
     train, train_dates = data[:(T-test_days)], dates[:(T-test_days)]
@@ -65,6 +69,7 @@ def rmse(pred, true):
     Returns:
         rwse (float): root mean squared error between the trajectories
     """
+    assert pred.shape==true.shape, "Shape mismatch"
     rwse = ((pred-true)**2).mean()**0.5
     return rwse
 
@@ -78,6 +83,7 @@ def mape(pred, true):
     Returns:
         mape (float): mean absolute percentage error between the trajectories (expressed as a decimal)
     """
+    assert pred.shape==true.shape, "Shape mismatch"
     mape = abs((true-pred)/true).mean()
     return mape
 
@@ -91,5 +97,6 @@ def mae(pred, true):
     Returns:
         mape (float): mean absolute  error between the trajectories (expressed as a decimal)
     """
+    assert pred.shape==true.shape, "Shape mismatch"
     mape = abs(true-pred).mean()
     return mae